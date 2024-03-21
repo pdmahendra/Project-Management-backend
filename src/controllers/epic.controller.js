0396f0275cacdc199cbe7af219c14d841a468b64
Pratik Mahendra <pratikkhatri520@gmail.com>
2024-03-21 20:34:06 +0530

    some correction and add delete epic function and route

--- src/controllers/epic.controller.js
+++ src/controllers/epic.controller.js
@@ -9,7 +9,7 @@ import { ApiError } from '../utils/ApiError.js';
 // siteName/projects/projectid/createEpic
 const createEpic = async (req, res) => {
     const { siteName, id } = req.params
-    const { project, epicName, summary, priority, assignee, reporter, description } = req.body;
+    const { project, epicName, summary, priority, assignee, reporter, description, status } = req.body;
 
     // console.log(siteName)
     // console.log(id)
@@ -42,6 +42,10 @@ const createEpic = async (req, res) => {
             reporter: req.user._id,
             description
         })
+
+        await findProject.updateOne({
+            $push: { epics: { epic: createEpic._id } }
+        }, { new: true })
 
         return res.status(201).json({ message: `epic created by ${req.user._id} under ${organization._id} in ${findProject._id}`, createEpic });
     }
@@ -99,34 +103,80 @@ const updateEpic = async (req, res) => {
     const { siteName, id, epicId } = req.params
     const { epicName, summary, priority, assignee, reporter, description } = req.body;
 
-    const organization = await Organization.findOne({ siteName });
-    if (!organization) {
-        return res.status(404).json("Organization not found")
-    }
-    const findProject = await Project.findOne({ _id: id });
-    if (!findProject) {
-        return res.status(404).json("Project not found")
-    }
+    try {
+        const organization = await Organization.findOne({ siteName });
+        if (!organization) {
+            return res.status(404).json("Organization not found")
+        }
+        const findProject = await Project.findOne({ _id: id });
+        if (!findProject) {
+            return res.status(404).json("Project not found")
+        }
 
-    const epic = await Epic.findOne({ _id: epicId });
-    if (!epic) {
-        throw new ApiError(400, "Epic with this id not exists in your project")
+        const epic = await Epic.findOne({ _id: epicId });
+        if (!epic) {
+            throw new ApiError(400, "Epic with this id not exists in your project")
+        }
+
+        if (String(req.user._id) !== String(organization.initialUser) || String(findProject.organizationId) !== String(organization._id)) {
+            throw new ApiError(401, "Unauthorized Accesss")
+        }
+
+        const updateEpic = await Epic.findByIdAndUpdate(epicId, {
+            epicName,
+            summary,
+            priority,
+            assignee,
+            reporter,
+            description
+        }, { new: true })
+
+        return res.status(200).json({ message: "Successfully updated Epic", updatedepic: updateEpic });
+    } catch (error) {
+        if (condition) {
+            return res.status(error.statusCode).json({ message: error.message })
+        } else {
+            console.error(error)
+            return res.status(500).json({ message: "Internal server error. Please try again later." })
+        }
     }
-
-    if (String(req.user._id) !== String(organization.initialUser) || String(findProject.organizationId) !== String(organization._id)) {
-        throw new ApiError(401, "Unauthorized Accesss")
-    }
-
-    const updateEpic = await Epic.findByIdAndUpdate(epicId, {
-        epicName,
-        summary,
-        priority,
-        assignee,
-        reporter,
-        description
-    }, { new: true })
-
-    return res.status(200).json({ message: "Successfully updated Epic", updatedepic: updateEpic });
 }
 
-export { createEpic, getEpicById, updateEpic }
+const deleteEpic = async (req, res) => {
+    const { siteName, id, epicId } = req.params;
+    const organization = await Organization.findOne({ siteName });
+    try {
+        if (!organization) {
+            return res.status(404).json("Organization not found")
+        }
+        const findProject = await Project.findOne({ _id: id });
+        if (!findProject) {
+            return res.status(404).json("Project not found")
+        }
+
+        if (String(req.user._id) !== String(organization.initialUser) || String(findProject.organizationId) !== String(organization._id)) {
+            throw new ApiError(401, "Unauthorized Accesss")
+        }
+
+        const epic = await Epic.findByIdAndDelete({ _id: epicId });
+        if (!epic) {
+            throw new ApiError(400, "Epic with this id not exists in your project")
+        }
+
+        await Project.updateOne(findProject, {
+            $pull: { epics: { epic: epic._id } }
+        }, { new: true })
+
+        return res.json({ message: "Epic deleted successfully", epic })
+    } catch (error) {
+        if (error instanceof ApiError) {
+            return res.status(error.statusCode).json({ message: error.message })
+        } else {
+            console.error(error);
+            return res.status(500).json({ message: "Internal server error. Please try again later." })
+        }
+    }
+}
+
+
+export { createEpic, getEpicById, updateEpic, deleteEpic }
--- src/routes/epic.routes.js
+++ src/routes/epic.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 const router = Router();
 import { verifyJwt } from '../middlewares/auth.middleware.js'
-import { createEpic, getEpicById, updateEpic } from '../controllers/epic.controller.js'
+import { createEpic, getEpicById, updateEpic, deleteEpic } from '../controllers/epic.controller.js'
 
 // router.use(verifyJwt)
 
 router.route('/:siteName/:id/createEpic').post(verifyJwt, createEpic)
-router.route('/:siteName/:id/epic/:epicId').get(verifyJwt, getEpicById)
-router.route('/:siteName/:id/epic/updateEpic/:epicId').get(verifyJwt, updateEpic)
+router.route('/:siteName/:id/getEpic/:epicId').get(verifyJwt, getEpicById)
+router.route('/:siteName/:id/updateEpic//:epicId').get(verifyJwt, updateEpic)
+router.route('/:siteName/:id/deleteEpic/:epicId').get(verifyJwt, deleteEpic)
 
 export default router
